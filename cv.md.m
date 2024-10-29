0c5b4950d91d3c80b967cd46a728b6b284bab4a0
Megan <mgli@ucsd.edu>
2024-10-28 21:16:57 -0700

    added cv page

---
+++ cv.md.m
@@ -0,0 +1,5 @@
+<div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
+  <iframe src="https://docs.google.com/document/d/e/2PACX-1vQG_uipTkXZjF_1POHmfAl0ILy8hRpDI5buNDeI1ac61LpW2C6Iv1HrPs7iCEtYFUefB7xkVAv4OkWr/pub?embedded=true" 
+          style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;">
+  </iframe>
+</div>
